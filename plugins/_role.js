61a7cc09c30cb5b64290cbffa429b59bcb665aff
agusfamilia13 <40955113+agusfamilia13@users.noreply.github.com>
2021-08-21 16:04:31 +0700

    autolevelup with canvas

--- plugins/_role.js
+++ plugins/_role.js
@@ -1,17 +1,17 @@
 let handler = m => m
 
 handler.before = function (m) {
-  let user = global.db.data.users[m.sender]
-        let role = (user.level <= 10) ? 'Warrior'
-          : ((user.level >= 10) && (user.level <= 20)) ? 'Elite'
-          : ((user.level >= 20) && (user.level <= 30)) ? 'Master'
-          : ((user.level >= 30) && (user.level <= 40)) ? 'Grandmaster'
-          : ((user.level >= 40) && (user.level <= 50)) ? 'Epic'
-          : ((user.level >= 50) && (user.level <= 60)) ? 'Legend'
-          : ((user.level >= 60) && (user.level <= 70)) ? 'Mythic'
-          : 'Mythical Glory'
-  user.role = role
-  return true
+    let user = global.db.data.users[m.sender]
+    let role = ((user.level >= 1) && (user.level <= 10)) ? 'Warrior'
+        : ((user.level >= 10) && (user.level <= 20)) ? 'Elite'
+            : ((user.level >= 20) && (user.level <= 30)) ? 'Master'
+                : ((user.level >= 30) && (user.level <= 40)) ? 'Grandmaster'
+                    : ((user.level >= 40) && (user.level <= 50)) ? 'Epic'
+                        : ((user.level >= 50) && (user.level <= 60)) ? 'Legend'
+                            : ((user.level >= 60) && (user.level <= 70)) ? 'Mythic'
+                                 : 'Mythical Glory'
+    user.role = role
+    return true
 }
 
 module.exports = handler
