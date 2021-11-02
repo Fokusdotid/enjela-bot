9c049a0152ebea322c7e2b387da62a73cc3f9417
Fokus Dot Id <40955113+Fokusdotid@users.noreply.github.com>
2021-11-02 17:52:12 +0700

    Update main.js

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ if (!global.db.data.sticker) global.db.data.sticker = {}
 
 global.DATABASE = global.db // Backwards Compatibility
 global.conn = new WAConnection()
+conn.version = [ 2, 2143, 3 ]
 let authFile = `${opts._[0] || 'aguz'}.familia.json`
 if (fs.existsSync(authFile)) conn.loadAuthInfo(authFile)
 if (opts['trace']) conn.logger.level = 'trace'
